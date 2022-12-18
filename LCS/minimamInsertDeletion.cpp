cbf490dedabea7828dd7950391f719c5ced05b6f
Asfak Shahrier <59991886+asfak27@users.noreply.github.com>
2022-12-18 15:45:53 +0600

    Add files via upload

---
+++ LCS/minimamInsertDeletion.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ll dp[7000][7000];
+ll lcs(string s1,string s2)
+{
+    ll i,j,x;
+    ll len1=s1.size();
+    ll len2=s2.size();
+    for(i=0; i<=s1.length(); i++)
+    {
+        for(j=0; j<=s2.length(); j++)
+        {
+            if(i==0||j==0)
+            {
+                dp[i][j]=0;
+            }
+            else if(s1[i-1]==s2[j-1])
+            {
+                dp[i][j]=dp[i-1][j-1]+1;
+            }
+            else
+            {
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    x=(len1-dp[len1][len2])+(len2-dp[len1][len2]);
+    return x;
+}
+void solve()
+{
+    ll n,m,j=0,y,i,k,x,c,d,l,r,mod=1e9+7;
+    string s1,s2;
+    cin>>s2>>s1;
+    x=lcs(s1,s2);
+    cout<<x<<endl;
+}
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)solve();
+    return 0;
+}
+
+
